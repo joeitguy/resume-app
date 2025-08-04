5a6fd13eefeaa5ef04c01e9704c06e2b1ead215b
joe <joeitguy@gmail.com>
2025-08-04 11:29:44 -0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/view/resume')
+def view_resume():
+    return render_template('resume.html')
+
+@app.route('/view/cover-letter')
+def view_cover_letter():
+    return render_template('cover_letter.html')
+
+@app.route('/view/skills')
+def view_skills():
+    return render_template('skills.html')
