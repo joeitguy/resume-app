2e1d3d747b251536f4656a73f8cd18e8ec701bb5
joe <joeitguy@gmail.com>
2025-08-09 08:37:27 -0700

    end points

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, render_template
 from resume.routes import resume_bp
+
+app = Flask(__name__, template_folder="static/templates")
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
 
 app = Flask(__name__, static_folder="client/build", static_url_path="")
 app.register_blueprint(resume_bp)
-
-@app.route("/")
-def serve_index():
-    return send_from_directory(app.static_folder, "index.html")
 
 app = Flask(__name__)
 app.register_blueprint(resume_bp)
